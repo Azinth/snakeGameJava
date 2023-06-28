3ed6533d0848d72e164f3afbc874a62c6a8be3e4
Azinth <gsv.jesus@gmail.com>
2023-06-28 16:07:05 -0300

    implements news methods

--- src/Snake/GamePanel.java
+++ src/Snake/GamePanel.java
@@ -1,13 +1,70 @@
 package Snake;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25; // size of the objects in the game
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE; // how many objects can fit in the game
+    static final int DELAY = 75; // speed of the game
+    final int x[] = new int[GAME_UNITS]; // x coordinates of the snake
+    final int y[] = new int[GAME_UNITS]; // y coordinates of the snake
+    int bodyParts = 6; // initial size of the snake
+    int applesEaten; // number of apples eaten
+    int appleX; // x coordinate of the apple
+    int appleY; // y coordinate of the apple
+    char direction = 'R'; // initial direction of the snake
+    boolean running = false; // is the game running?
+    Timer timer; // timer for the game
+    Random random; // random number generator
+
+
+    GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
+
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
     }
 }
--- src/Snake/Main.java
+++ src/Snake/Main.java
@@ -2,6 +2,8 @@ package Snake;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        GameFrame frame = new GameFrame();
+
     }
 }
